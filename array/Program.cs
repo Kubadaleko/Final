e9446d88a8139b9a851719cfb4f98b648a551478
Kubadaleko <7757535@mail.ru>
2022-12-26 15:32:00 +0300

    Написал программу

--- array/Program.cs
+++ array/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// /* Задача: Написать программу, которая из имеющегося массива строк формирует 
+// новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.*/
+
+string[] arrayWords1 = { "Иван", "Ольга", "Рот", "Бар", "Кот", "Вера", "Кремль" };
+string[] arrayWords2 = new string[arrayWords1.Length];
+
+void newArrayWords(string[] array1, string[] array2)
+{
+    int k = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[k] = array1[i];
+            k++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+newArrayWords(arrayWords1, arrayWords2);
+PrintArray(arrayWords2);
+
+
+// Еще вариант решения
+
+// string[] arrayWords1 = { "Иван", "Ольга", "Рот", "Бар", "Кот", "Вера", "Кремль" };
+// string[] arrayWords2 = new string[arrayWords1.Length];
+// for (int i = 0; i < arrayWords1.Length; i++)
+// {
+//     if (arrayWords1[i].Length <= 3)
+//         arrayWords2[i] = arrayWords1[i];
+// }
+// System.Console.WriteLine(string.Join(" ", arrayWords2));
